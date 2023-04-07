b3020fdfa23bb9f82c6c1f88e7913f361f641c73
SudyarovAlexey <JohnDoe1980>
2023-04-07 23:34:36 +0400

    The code is corrected according to the comments of the code review.

--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,41 +1,39 @@
-import com.fasterxml.jackson.databind.JsonNode;
 import io.qameta.allure.Description;
+import models.Pokemon;
+import models.Result;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import steps.Steps;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.lessThan;
 
-public class ApiTests extends BaseTest {
+public class ApiTests {
 
     @Test
-    @Description("Сравнение веса покемона Pidgeotto и веса покемона Rattata")
-    public void compareWeights() {
-        JsonNode pidgeottoNode = Steps.getPokemon(requestSpecification, "pidgeotto");
-        JsonNode rattataNode = Steps.getPokemon(requestSpecification, "rattata");
-        int weightPidgeotto = Steps.getPokemonWeight(pidgeottoNode);
-        int weightRattata = Steps.getPokemonWeight(rattataNode);
-        org.hamcrest.MatcherAssert.assertThat(weightRattata, lessThan(weightPidgeotto));
+    @Description("Сравнение веса покемона Pidgeotto и веса покемона Rattata. " +
+            "Проверка наличия способности 'run-away' у покемона Rattata")
+    public void compareWeightsAndCheckAbility() {
+        Pokemon pidgeotto = Steps.getPokemon("pidgeotto");
+        Pokemon rattata = Steps.getPokemon("rattata");
+        org.hamcrest.MatcherAssert.assertThat(rattata.getWeight(), lessThan(pidgeotto.getWeight()));
+        Assert.assertTrue(rattata.getAbilities()
+                .stream()
+                .map(e -> e.getAbility().getName()).collect(Collectors.toList())
+                .contains("run-away"));
     }
 
     @Test
-    @Description("Проверка наличия способности run-away у покемона Rattata")
-    public void hasAbilityRunAway() {
-        JsonNode rattataNode = Steps.getPokemon(requestSpecification, "rattata");
-        List<String> list = Steps.getAbilityList(rattataNode);
-        Assert.assertTrue(list.contains("run-away"));
-    }
-
-    @Test
-    @Description("Получение списка покемонов и проверка наличия их имен (not null)")
-    public void checkList() {
-        int countPokemon = Steps.getSizePokemonList(requestSpecification);
-        for (int i = 1; i < countPokemon + 100; i = i + 100) {
-            JsonNode resourceJson = Steps.getPokemonSublist(requestSpecification, i);
-            List<String> nameList = Steps.getNameList(resourceJson);
-            nameList.forEach(Assert::assertNotNull);
-        }
+    @Description("Проверка ограничения списка (limit) покемонов и проверка наличия имени (name) у каждого покемона в" +
+            "ограниченном списке")
+    public void checkLimitAndNameAvailability() {
+        int limit = 10;
+        Map<String, Integer> requestParams = Map.of("limit", limit);
+        List<Result> resultList = Steps.getPokemonList(requestParams).getResults();
+        Assert.assertEquals(resultList.size(), limit);
+        resultList.stream().map(Result::getName).collect(Collectors.toList()).forEach(Assert::assertNotNull);
     }
 }
--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -4,21 +4,28 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.qameta.allure.Step;
-import io.restassured.specification.RequestSpecification;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import models.Pokemon;
 import models.PokemonResource;
-import models.Result;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Map;
+
+import static io.restassured.RestAssured.given;
 
 public class Steps {
 
+    private static final AllureRestAssured allureRestAssured = new AllureRestAssured();
+
     @Step("Получение данных покемона")
-    public static JsonNode getPokemon(RequestSpecification requestSpecification, String value) {
-        return requestSpecification
+    public static Pokemon getPokemon(String value) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode pokemonJson = given()
+                .filter(allureRestAssured)
+                .baseUri("https://pokeapi.co/api/v2/")
+                .contentType(ContentType.JSON)
                 .pathParam("pathParam", value)
                 .when()
                 .log().all()
@@ -26,74 +33,33 @@ public class Steps {
                 .then()
                 .statusCode(200)
                 .extract().as(JsonNode.class);
-    }
-
-    @Step("Получение значения веса покемона")
-    public static int getPokemonWeight(JsonNode node) {
-        ObjectMapper mapper = new ObjectMapper();
-        int weight = 0;
         try {
-            weight = mapper.readValue(mapper.treeAsTokens(node), new TypeReference<Pokemon>() {
-            }).getWeight();
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-        return weight;
-    }
-
-    @Step("Проверка наличия способности")
-    public static List<String> getAbilityList(JsonNode node) {
-        ObjectMapper mapper = new ObjectMapper();
-        List<String> resultList = new ArrayList<>();
-        try {
-            Pokemon pokemon = mapper.readValue(mapper.treeAsTokens(node), new TypeReference<>() {
+            return mapper.readValue(mapper.treeAsTokens(pokemonJson), new TypeReference<>() {
             });
-            resultList = pokemon
-                    .getAbilities()
-                    .stream()
-                    .map(e -> e.getAbility().getName())
-                    .collect(Collectors.toList());
-        } catch (
-                IOException e) {
-            e.printStackTrace();
-        }
-        return resultList;
-    }
-
-    @Step("Получение размера полного списка покемонов")
-    public static int getSizePokemonList(RequestSpecification requestSpecification) {
-        return requestSpecification
-                .when()
-                .log().all()
-                .get("pokemon")
-                .then()
-                .extract().response().getBody()
-                .as(PokemonResource.class)
-                .getCount();
-    }
-
-    @Step("Получение части списка покемонов")
-    public static JsonNode getPokemonSublist(RequestSpecification requestSpecification, int count) {
-        return requestSpecification
-                .when()
-                .log().all()
-                .get("pokemon?offset=" + count + "&limit=1000")
-                .then()
-                .extract().as(JsonNode.class);
-    }
-
-    @Step("Получение списка имен покемонов")
-    public static List<String> getNameList(JsonNode resourceJson) {
-        ObjectMapper mapper = new ObjectMapper();
-        List<String> nameList = new ArrayList<>();
-        try {
-            PokemonResource resource = mapper.readValue(mapper.treeAsTokens(resourceJson), new TypeReference<>() {
-            });
-            nameList = resource.getResults().stream().map(Result::getName).collect(Collectors.toList());
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return nameList;
+        return new Pokemon();
+    }
+
+    @Step("Получение списка покемонов")
+    public static PokemonResource getPokemonList(Map<String, Integer> requestParams) {
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode resourceJson = given()
+                .filter(allureRestAssured)
+                .baseUri("https://pokeapi.co/api/v2/")
+                .contentType(ContentType.JSON)
+                .when()
+                .log().all()
+                .get("pokemon?limit=" + requestParams.get("limit"))
+                .then()
+                .extract().as(JsonNode.class);
+        try {
+            return mapper.readValue(mapper.treeAsTokens(resourceJson), new TypeReference<>() {
+            });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new PokemonResource();
     }
 }
