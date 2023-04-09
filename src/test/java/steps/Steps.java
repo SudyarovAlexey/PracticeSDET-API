682e16d6bc59aca7ed88d9e24f69b85361d50073
SudyarovAlexey <JohnDoe1980>
2023-04-09 14:47:53 +0400

    The code is corrected according to the comments of the code review. Part II.

--- src/test/java/steps/Steps.java
+++ src/test/java/steps/Steps.java
@@ -7,25 +7,29 @@ import io.qameta.allure.Step;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
 import models.Pokemon;
 import models.PokemonResource;
 
 import java.io.IOException;
 import java.util.Map;
 
-import static io.restassured.RestAssured.given;
-
 public class Steps {
 
     private static final AllureRestAssured allureRestAssured = new AllureRestAssured();
+    private static final RequestSpecification requestSpecification;
+
+    static {
+        requestSpecification = RestAssured.given()
+                .filter(allureRestAssured)
+                .baseUri("https://pokeapi.co/api/v2/")
+                .contentType(ContentType.JSON);
+    }
 
     @Step("Получение данных покемона")
     public static Pokemon getPokemon(String value) {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode pokemonJson = given()
-                .filter(allureRestAssured)
-                .baseUri("https://pokeapi.co/api/v2/")
-                .contentType(ContentType.JSON)
+        JsonNode pokemonJson = requestSpecification
                 .pathParam("pathParam", value)
                 .when()
                 .log().all()
@@ -45,10 +49,7 @@ public class Steps {
     @Step("Получение списка покемонов")
     public static PokemonResource getPokemonList(Map<String, Integer> requestParams) {
         ObjectMapper mapper = new ObjectMapper();
-        JsonNode resourceJson = given()
-                .filter(allureRestAssured)
-                .baseUri("https://pokeapi.co/api/v2/")
-                .contentType(ContentType.JSON)
+        JsonNode resourceJson = requestSpecification
                 .when()
                 .log().all()
                 .get("pokemon?limit=" + requestParams.get("limit"))
